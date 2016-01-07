234c6253681f75607c712319aa292c081e4c0ae5
Besik13 <egor.besik@gmail.com>
2016-01-07 22:26:39 +0200

    Check the availability of connections

--- app/src/main/java/com/schedule/risebes/schedule/server/ServerCommand.java
+++ app/src/main/java/com/schedule/risebes/schedule/server/ServerCommand.java
@@ -1,10 +1,41 @@
 package com.schedule.risebes.schedule.server;
 
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.ExecutionException;
+
 public class ServerCommand {
 
     public static boolean isAvailable() {
-        //TODO: Create connectify check
-        return true;
+        boolean res = false;
+        try {
+            res = (boolean) new AsyncTask() {
+                public boolean connect;
+
+                @Override
+                protected Boolean doInBackground(Object[] params) {
+                    try {
+                        Socket s = new Socket(ServerSettings.SERVER_ADDRESS, ServerSettings.TCP_SERVER_PORT);
+                        if (s.isConnected()) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+
+                }
+            }.execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return res;
     }
 
     public final static String getLoginString(String email, String password) {
