b837a261f4bb5fffdd6f59572efd8ba2fbafcce1
Besik13 <egor.besik@gmail.com>
2016-01-07 22:25:44 +0200

    Update server settings

--- app/src/main/java/com/schedule/risebes/schedule/server/ServerSettings.java
+++ app/src/main/java/com/schedule/risebes/schedule/server/ServerSettings.java
@@ -1,7 +1,7 @@
 package com.schedule.risebes.schedule.server;
 
 public class ServerSettings {
-    public final static String SERVER_ADDRESS="http://192.168.0.100:";
+    public final static String SERVER_ADDRESS="192.168.0.100";
     public final static int TCP_SERVER_PORT=8080;
-    public final static String SERVER_URL=SERVER_ADDRESS+TCP_SERVER_PORT;
+    public final static String SERVER_URL="http://"+SERVER_ADDRESS+":"+TCP_SERVER_PORT;
 }
