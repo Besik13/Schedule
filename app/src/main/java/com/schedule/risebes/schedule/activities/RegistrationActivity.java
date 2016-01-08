738c4872065abf176e4011e16d929d492d9540d3
Besik13 <egor.besik@gmail.com>
2016-01-08 16:41:24 +0200

    Remove group select from registration activity

--- app/src/main/java/com/schedule/risebes/schedule/activities/RegistrationActivity.java
+++ app/src/main/java/com/schedule/risebes/schedule/activities/RegistrationActivity.java
@@ -3,19 +3,14 @@ package com.schedule.risebes.schedule.activities;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.schedule.risebes.schedule.R;
-import com.schedule.risebes.schedule.rest.Groups;
 import com.schedule.risebes.schedule.rest.NewUser;
 import com.schedule.risebes.schedule.server.ServerCommand;
 
@@ -25,20 +20,14 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
-
-public class RegistrationActivity extends AppCompatActivity implements TextWatcher {
-    private Groups group;
+public class RegistrationActivity extends AppCompatActivity {
     private View mProgressView;
     private EditText mEmail;
     private EditText mPassword;
     private EditText mName;
     private EditText mSurname;
-    private Spinner mGroup;
     private Button buttonReg;
 
     private UserRegistration userRegistration = new UserRegistration();
@@ -51,7 +40,6 @@ public class RegistrationActivity extends AppCompatActivity implements TextWatch
         regListeners();
         mProgressView = findViewById(R.id.reg_progress);
         showProgress(true);
-        new GetGroups().execute();
     }
 
     private void initFields()
@@ -61,7 +49,6 @@ public class RegistrationActivity extends AppCompatActivity implements TextWatch
         mPassword = (EditText) findViewById(R.id.reg_password);
         mName = (EditText) findViewById(R.id.reg_name);
         mSurname = (EditText) findViewById(R.id.reg_surname);
-        mGroup = (Spinner) findViewById(R.id.reg_group);
     }
 
     private void regListeners()
@@ -69,7 +56,7 @@ public class RegistrationActivity extends AppCompatActivity implements TextWatch
         buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(testFieldsOfValid()) {
+                if (testFieldsOfValid()) {
                     showProgress(true);
                     userRegistration.execute();
                 }
@@ -104,10 +91,6 @@ public class RegistrationActivity extends AppCompatActivity implements TextWatch
             focusView = mSurname;
             cancel = true;
         }
-        if (mGroup.getSelectedItem()==null) {
-            focusView = mGroup;
-            cancel = true;
-        }
         if (cancel) {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
@@ -123,71 +106,7 @@ public class RegistrationActivity extends AppCompatActivity implements TextWatch
         user.setPassword(mPassword.getText().toString());
         user.setName(mName.getText().toString());
         user.setSurename(mSurname.getText().toString());
-        user.setGroup(getSelectedGroupId());
         return user;
-    }
-    private Integer getSelectedGroupId()
-    {
-        String groupName = mGroup.getSelectedItem().toString();
-        for(Map.Entry<Integer,String> entry:group.getGroups().entrySet())
-        {
-            if(entry.getValue().trim().equalsIgnoreCase(groupName))
-            {
-                return entry.getKey();
-            }
-        }
-        return null;
-    }
-
-    private class GetGroups extends AsyncTask<Void, Void, Groups>
-    {
-        @Override
-        protected Groups doInBackground(Void... params) {
-            try {
-                final String url = ServerCommand.getGroupsString();
-                RestTemplate restTemplate = new RestTemplate();
-                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-                return restTemplate.getForObject(url, Groups.class);
-            } catch (HttpClientErrorException e) {
-
-            } catch (ResourceAccessException e) {
-
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Groups groups) {
-            if(groups!=null) {
-                group = groups;
-                Spinner autoCompleteGroup;
-                String[] listOfGroups = new String[groups.getGroups().size()];
-                groups.getGroups().values().toArray(listOfGroups);
-                autoCompleteGroup = (Spinner) findViewById(R.id.reg_group);
-                autoCompleteGroup.setAdapter(new ArrayAdapter<>(RegistrationActivity.this,
-                        android.R.layout.simple_spinner_dropdown_item, listOfGroups));
-            }else
-            {
-                //TODO Make some info message
-            }
-            showProgress(false);
-        }
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
-
     }
 
     private class UserRegistration extends AsyncTask<Void, Void, NewUser>
